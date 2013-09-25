ad569daa4596686c1be80548d51490595635e5f7
3Easy <hello@3easy.org>
2013-09-25 16:53:39 +1000

    Settings CSS and things seperated out into own dir

--- iotas/www/apps/settings/settings.js
+++ iotas/www/apps/settings/settings.js
@@ -8,9 +8,7 @@ function settings() {
 	// Start App
 	function appStart() {
 		console.log("settings.appStart");
-		//$('head').append('<meta name="viewport" content="width=device-width, initial-scale=1">');
-		//$('head').append('<meta name="apple-mobile-web-app-capable" content="yes" />');
-		$('head').append('<link rel="stylesheet" href="settings.css" />'); // Muy importante!
+		$('head').append('<link rel="stylesheet" href="settings.css" />');
 	}
 	
 	// Quit App
