950627a3ad75eab118a028dff8171c036feae267
mpesce <mpesce@gmail.com>
2013-09-29 13:31:00 +1000

    Basic implementation of JSONP App of the Day in Rainbow app

--- iotas/www/apps/rainbow/rainbow.js
+++ iotas/www/apps/rainbow/rainbow.js
@@ -7,11 +7,13 @@ function rainbow() {
 	this.demoStart = demoStart;
 	this.demoStop = demoStop;
 	this.running = false;
+	this.loadTest = loadTest;
 		
 	// Start App
 	function appStart() {
 		console.log("rainbow.appStart");
 		$("head").append('<link rel="stylesheet" href="rainbow.css" />');
+		loadTest();
 	}
 	
 	// Quit App
@@ -22,10 +24,32 @@ function rainbow() {
 			theApp.running = false;
 		}
 	}
+
+	function loadrx() {
+		console.log("loadrx");
+	}
+
+	// Have some fun with JSONP.  What's the worst that could happen?
+	function loadTest() {
+		console.log("loadTest");
+		$.ajax({
+	        url: 'http://repo.moorescloud.com/holiday/apps/appoftheday/appoftheday.js',
+	        dataType: 'jsonp',
+	        crossDomain: true,
+	        success: function(data) {
+	        	var d = data;
+	        	console.log("SUCCESS data:");
+	        	console.log(data);
+	        },
+	        error: function(e) {
+	        	console.log("Error on loadTest JSONP");	 // This is happening, who knows why?
+	        }
+	       });
+	}	
 	
 	// Start Demo 
 	function demoStart() {
-		
+
 		//
 		// Insert IoTAS Code
 		//
